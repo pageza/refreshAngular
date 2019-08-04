4ea118a4edb8a1d209790b4e6b2f5c172e6b2752
pageza <zap.freelance@gmail.com>
2019-08-04 16:45:44 -0600

    adding first part of MEAN refresher

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'refreshAngular';
+  number = 10;
+  name = 'Bill Gates';
+  user = {
+    firstName: 'Darth',
+    lastName: 'Vader'
+  }
+
+  loggedIn = false;
+
+  numbers = [1,2,3,4,5];
+
+  color = 'green';
+
+  count = 0;
+  onclick(data) {
+    console.log("Clicked the damned button!", data)
+  }
+
+  person = { email: ''}
+  persons = [];
+  onSubmit(){
+    this.persons.push(this.person);
+    this.person = { email: ''};
+  }
 }
